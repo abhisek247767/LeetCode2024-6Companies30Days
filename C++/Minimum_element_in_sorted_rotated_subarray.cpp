82a89dfe2e4d5f7b3549d79e93328eaaa8da06dd
Srusti Gourani <79088553+srusti610@users.noreply.github.com>
2023-10-12 17:32:03 +0530

    Update Minimum_element_in_sorted_rotated_subarray.cpp

--- C++/Minimum_element_in_sorted_rotated_subarray.cpp
+++ C++/Minimum_element_in_sorted_rotated_subarray.cpp
@@ -1,4 +1,11 @@
-// C++ code to implement the approach
+Problem statement:
+
+Given a sorted array arr[] (may be distinct or may contain duplicates) of size N that is rotated at some unknown point, the task is to find the minimum element in it. 
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
 #include <bits/stdc++.h>
 using namespace std;
