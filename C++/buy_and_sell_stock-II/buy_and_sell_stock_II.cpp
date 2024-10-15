56a0e981e39cef978382f065c16d317564c7b104
Yashvi Mehta <ydmehta3115@gmail.com>
2024-10-16 01:43:26 +0530

    Modified best time to buy and sell stocks II

--- C++/buy_and_sell_stock-II/buy_and_sell_stock_II.cpp
+++ C++/buy_and_sell_stock-II/buy_and_sell_stock_II.cpp
@@ -9,7 +9,7 @@
 // Example 1:
 
 // Input: prices = [7,1,5,3,6,4]
-// Output: 7
+// Output:7
 // Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
 // Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 // Total profit is 4 + 3 = 7.
